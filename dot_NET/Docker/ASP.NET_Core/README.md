cb5df12d1df8314d7a01091f29de7562cb18e24b
jenks.gibbons <jenks.gibbons@COMP10581.local>
2019-03-15 16:16:12 -0700

    Initial commit

---
+++ dot_NET/Docker/ASP.NET_Core/README.md
@@ -0,0 +1,10 @@
+### Setup:
+
+Update the `docker-compose.yml` and add your Datadog API key.
+
+### Run Command:
+
+`docker-compose run --build -d`
+
+### Produce load
+Visit http://localhost:5004/
---
+++ dot_NET/Docker/ASP.NET_Core/TripTracker.BackService/Controllers/TripsController.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite.Internal.UrlMatches;
+using Microsoft.EntityFrameworkCore;
+using TripTracker.BackService.Data;
+using TripTracker.BackService.Models;
+
+namespace TripTracker.BackService.Controllers
+{
+
+	[Route("api/[controller]")]
+	public class TripsController : Controller
+	{
+
+		TripContext _context;
+		public TripsController(TripContext context)
+		{
+			_context = context;
+		}
+
+
+		// GET api/Trips
+		[HttpGet]
+		public async Task<IActionResult> GetAsync()
+		{
+			var trips = await _context.Trips
+				.AsNoTracking()
+				.ToListAsync();
+			return Ok(trips);
+		}
+
+		// GET api/Trips/5
+		[HttpGet("{id}")]
+		public Trip Get(int id)
+		{
+			return _context.Trips.Find(id);
+		}
+
+		// POST api/Trips
+		[HttpPost]
+		public IActionResult Post([FromBody]Trip value)
+		{
+
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			_context.Trips.Add(value);
+			_context.SaveChanges();
+
+			return Ok();
+
+		}
+
+		// PUT api/Trips/5
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutAsync(int id, [FromBody]Trip value)
+		{
+
+			if (!_context.Trips.Any(t => t.Id == id))
+			{
+				return NotFound();
+			}
+
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			//what about nulls?
+			_context.Trips.Update(value);
+			await _context.SaveChangesAsync();
+
+			return Ok();
+		}
+
+		// DELETE api/Trips/5
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+            var myTrip = _context.Trips.Find(id);
+
+            if (myTrip == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trips.Remove(myTrip);
+            _context.SaveChanges();
+
+            // DELETE FROM Trips WHERE id=?
+
+            return NoContent();
+		}
+	}
+}
